197a079e7b87d257f29aeca524e7e6f0625bfca5
Damian Tarnawsky <damian@builder.io>
2025-01-27 08:47:28 -0800

    fix: use ng-recaptcha

--- src/app/components/get-started.component.ts
+++ src/app/components/get-started.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, inject, Input, OnInit } from "@angular/core";
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS, MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -6,6 +6,7 @@ import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import { states } from "./us-states";
 import { creditScores } from "./credit-scores";
+import { RECAPTCHA_V3_SITE_KEY, RecaptchaModule, ReCaptchaV3Service } from "ng-recaptcha-2";
 import { environment } from "~/environments/environment";
 
 interface Form {
@@ -20,18 +21,25 @@ interface Form {
   standalone: true,
   templateUrl: './get-started.component.html',
   styleUrl: './get-started.component.scss',
-  providers: [{ provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }],
-  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } },
+    { provide: RECAPTCHA_V3_SITE_KEY, useValue: environment.reCAPTCHASiteKey },
+    ReCaptchaV3Service
+  ],
+  imports: [FormsModule, MatFormFieldModule, MatInputModule, MatSelectModule, MatButtonModule, RecaptchaModule],
 })
 export class GetStartedComponent implements OnInit {
   private scriptElement: HTMLScriptElement | null = null;
+  private recaptchaV3Service = inject(ReCaptchaV3Service);
   @Input() question1: string = 'What is your name?';
 
   states = states;
   creditscores = creditScores;
 
   ngOnInit() {
-    this.loadScript('https://www.google.com/recaptcha/api.js');
+    //this.loadScript('https://www.google.com/recaptcha/api.js');
+    this.recaptchaV3Service.execute('importantAction')
+      .subscribe((token) => this.handleToken(token));
   }
   form: Form = {
     propertyValue: '',
@@ -47,7 +55,10 @@ export class GetStartedComponent implements OnInit {
     document.body.appendChild(this.scriptElement);
   }
 
-  onSubmit(token: any) {
+  handleToken(token: any) {
+    console.log(token);
+  } 
+  onSubmit(token: any) {    
     console.log(token);
     console.log(this.form);
     // Add your validation logic
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,4 +2,5 @@
 export const environment = {
   production: false,
   builderApiKey: "b0436771b9364792bfe150ddca471d51",
+  reCAPTCHASiteKey: "6Lf08MQqAAAAAGbQidBfm3NanIQvx4zVIT5F-pyn"
 };
